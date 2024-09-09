c2faf94a6bb04cc72efe46adc395028cde13d296
Christian Eriksson <christian.eriksson@live.se>
2024-09-09 21:34:24 +0200

    Implement helper function for finding config files
    The process of finding completion config files and directories is a
    little involved and it obscures the process of generating completion
    suggestions. Further, we will want to find other config files, like file
    for flag completions pretty soon. Since these future completion files
    will be stored in the file tree following a similar schema we can use
    the same logic. All this taken into account breaking the functionality
    into a helper function seems reasonable.

--- cli-completion.bash
+++ cli-completion.bash
@@ -2,6 +2,46 @@
 
 [ -z "$CLI_NAME" ] && echo "ERROR: could not load cli completions" && return 1
 
+# will stop searching if it finds any flags, so filter flags before searching
+# for sub-commands
+_search_completion_file() {
+    local file_type="$1"
+    shift
+    local raw_command_chain=("$@")
+    local executable_path=$(which "${raw_command_chain[0]}")
+    local script_path=$(readlink $executable_path)
+    local source_dir=$(realpath $(dirname "$script_path"))
+
+    local file_path="$source_dir/commands/"
+    local counter=0
+    for command in "${raw_command_chain[@]}"; do
+        [ -z "$command" ] && break
+        [ "$counter" -eq 0 ] && counter=$(($counter + 1)) && continue
+        case "$command" in
+            -*)
+                break
+            ;;
+            *)
+                if [ -d "$file_path$command-commands" ]; then
+                    file_path="$file_path$command-commands/"
+                else
+                    file_path="$file_path$command-"
+                fi
+            ;;
+        esac
+        counter=$(($counter + 1))
+        if [ "$counter" -eq $(("${#raw_command_chain[@]}" -1)) ]; then
+            local second_to_last_option=$file_path
+        fi
+    done
+    local base_path="${file_path%commands\/}"
+    file_path="$base_path$file_type.txt"
+    if [ ! -f "$file_path" ]; then
+        file_path="$second_to_last_option"
+    fi
+    echo "${file_path}"
+}
+
 _cli_completions() {
     local filtered_words=()
 
@@ -10,7 +50,6 @@ _cli_completions() {
             filtered_words+=("$word")
         fi
     done
-
     local number_of_inputs="${#filtered_words[@]}"
 
     # 1st (base) command is not finished
@@ -31,43 +70,32 @@ _cli_completions() {
     # first set of sub-commands finished, on n-th level, looking for 1+n-th
     # level of sub-commands
     if [ "$number_of_inputs" -gt "2" ]; then
-        local base_sub_commands_dir="$source_dir/commands"
-        local nth_sub_command_level=$(($number_of_inputs -1))
-        local nth_plus_one_sub_command_dir=$base_sub_commands_dir
-        local nth_sub_command_dir=$base_sub_commands_dir
-        local command_completion_file=""
-        local i=1
-        local counter=1
-        while [ $i -lt $nth_sub_command_level ]; do
-            if [ -d "$nth_plus_one_sub_command_dir/${filtered_words[$i]}-commands" ]; then
-                nth_plus_one_sub_command_dir="$nth_plus_one_sub_command_dir/${filtered_words[$i]}-commands"
-                if [ $i -lt $(($nth_sub_command_level - 1)) ]; then
-                    nth_sub_command_dir="$nth_sub_command_dir/${filtered_words[$i]}-commands"
-                fi
-                counter=$(($counter + 1))
-            else
-                command_completion_file="$command_completion_file${filtered_words[$i]}-"
-            fi
-            i=$(($i + 1))
-        done
+        local command_count=$(($number_of_inputs -1))
+
+        local completions_file=($(_search_completion_file "completions" "${filtered_words[@]}"))
+        local completions_directory="${completions_file%completions.txt}commands"
+        if [ ! -d "$completions_directory" ] && [ ! -f "$completions_file" ]; then
+            case "$completions_file" in
+                */)
+                    completions_directory=$completions_file
+                    completions_file="${completions_file%commands\/}completions.txt"
+                ;;
+                *)
+                    completions_file="${completions_file}completions.txt"
+                ;;
+            esac
+        fi
 
         local completions=""
-        if [ $counter -eq $nth_sub_command_level ] && [ -d "$nth_plus_one_sub_command_dir" ]; then
-            completions="$completions $(find "$nth_plus_one_sub_command_dir/" -maxdepth 1 -type f -executable -execdir sh -c 'f=$(basename $0); printf "%s\n" "${f%.*}"' {} ';' | tr "\n" " ")"
+        if [ -n "$completions_directory" ] && [ -d "$completions_directory" ]; then
+            completions="$completions $(find "$completions_directory/" -maxdepth 1 -type f -executable -execdir sh -c 'f=$(basename $0); printf "%s\n" "${f%.*}"' {} ';' | tr "\n" " ")"
         fi
 
-        current_command="${filtered_words[$(($nth_sub_command_level - 1))]}"
-        sub_command_completion_file="$nth_sub_command_dir/$current_command-completions.txt"
-        if [ $counter -ge $(($nth_sub_command_level - 1)) ] && [ -f "$sub_command_completion_file" ]; then
-            completions="$completions $(cat "$sub_command_completion_file")"
+        if [ -n "$completions_file" ] && [ -f "$completions_file" ]; then
+            completions="$completions $(cat "$completions_file")"
         fi
 
-        multi_sub_command_completion_file="$nth_sub_command_dir/${command_completion_file}completions.txt"
-        if [ -f "$multi_sub_command_completion_file" ]; then
-            completions="$completions $(cat "$multi_sub_command_completion_file")"
-        fi
-
-        COMPREPLY=($(compgen -W "$completions" -- "${filtered_words[$nth_sub_command_level]}"))
+        COMPREPLY=($(compgen -W "$completions" -- "${filtered_words[$command_count]}"))
     fi
 }
 
