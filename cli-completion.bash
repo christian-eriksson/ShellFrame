dc6f822ad6b6e864b26395a39701e66164f15509
Christian Eriksson <christian.eriksson@live.se>
2024-09-15 12:48:41 +0200

    REFACTOR: Simplify logic for input parsing
    The parsing of the input, where we determine what flags, commands and
    completion hint has been provided is more complex than it needs to be.
    We really just need to pick the last item of the input array and then
    split the rest into flags and commands.
    
    With that simplification we can also remove some of the workaround used
    when finding completion files and directories. All in all the completion
    script should get much more easy to follow.

--- cli-completion.bash
+++ cli-completion.bash
@@ -7,13 +7,12 @@
 _search_completion_file() {
     local file_type="$1"
     shift
-    local raw_command_chain=("$@")
-    local executable_path=$(which "${raw_command_chain[0]}")
+    local commands=("$@")
+    local executable_path=$(which "${commands[0]}")
     local script_path=$(readlink $executable_path)
     local source_dir=$(realpath $(dirname "$script_path"))
 
-    local chain_length="${#raw_command_chain[@]}"
-    if [ "$chain_length" -eq "2" ]; then
+    if [ "${#commands[@]}" -eq "1" ]; then
         local script_file=$(basename $script_path)
         local script_name="${script_file%.*}"
         file_path="$source_dir/$script_name-$file_type.txt"
@@ -22,10 +21,8 @@ _search_completion_file() {
     fi
 
     local file_path="$source_dir/commands/"
-    local past_first="false"
-    for command in "${raw_command_chain[@]}"; do
-        [ -z "$command" ] && break
-        [ "$past_first" = "false" ] && past_first="true" && continue
+    unset commands[0]
+    for command in "${commands[@]}"; do
         case "$command" in
             -*)
                 break
@@ -39,44 +36,25 @@ _search_completion_file() {
             ;;
         esac
     done
-    local base_path="${file_path%commands\/}"
-    file_path="$base_path$file_type.txt"
+    file_path="${file_path%commands\/}$file_type.txt"
     echo "${file_path}"
 }
 
 _cli_completions() {
+    local input_array=("${COMP_WORDS[@]}")
+    local completion_hint="${input_array[-1]}"
+    unset input_array[-1]
+
     local commands_provided=()
-    local word_counter="0"
-    local flag_filtered_at_position="0"
     local flags_to_current_command=()
-    local last_flag_piece=""
-    for word in "${COMP_WORDS[@]}"; do
-        word_counter=$(($word_counter + 1))
-        if [[ "$word" != -* ]]; then
-            commands_provided+=("$word")
-            [ -n "$word" ] && flags_to_current_command=()
+    for word in "${input_array[@]}"; do
+        if [[ "$word" = -* ]]; then
+            flags_to_current_command+=("$word")
         else
-            if [ "${#word}" -eq "2" ]; then
-                flags_to_current_command+=("$word")
-            fi
-            parital_flag_at_position="$word_counter"
-            last_flag_piece=$word
+            commands_provided+=("$word")
+            flags_to_current_command=()
         fi
     done
-    local flag_hint=""
-    if [ "$parital_flag_at_position" -eq  "${#COMP_WORDS[@]}" ]; then
-        flag_hint="$last_flag_piece"
-        commands_provided+=("");
-    fi
-
-    local number_of_commands="${#commands_provided[@]}"
-    if [ "$number_of_commands" -lt "2" ]; then
-        return
-    fi
-
-    local completion_hint="${commands_provided[-1]}"
-    [ -n "$flag_hint" ] && completion_hint="$flag_hint"
-    [ -n "$completion_hint" ] && unset commands_provided[-1] && commands_provided+=("")
 
     local flag_completions_file=($(_search_completion_file "flags" "${commands_provided[@]}"))
     local completions=""
@@ -88,20 +66,19 @@ _cli_completions() {
     # as something like 'cli -v -h{tab}' will not be auto completed if '-h' is
     # removed from the completion list. So we only remove if we are starting a
     # new flag or are not providing a flag.
-    if [ "$completion_hint" = "-" ] || [ -z "$flag_hint" ]; then
+    if ! ([[ "$completion_hint" = -* ]] && [ "${#completion_hint}" -eq 2 ]); then
         for flag in "${flags_to_current_command[@]}"; do
             completions=${completions/$flag/}
         done
     fi
 
-    if [ -n "$flag_hint" ]; then
+    if [[ "$completion_hint" = -* ]]; then
         COMPREPLY=($(compgen -W "$completions" -- "$completion_hint"))
         return
     fi
 
     local completions_file=($(_search_completion_file "completions" "${commands_provided[@]}"))
-    local command_count_wo_completion_hint=$(($number_of_commands -1))
-    if [ "$command_count_wo_completion_hint" -eq 1 ]; then
+    if [ "${#commands_provided[@]}" -eq 1 ]; then
         local executable_path=$(which "${commands_provided[0]}")
         local script_path=$(readlink $executable_path)
         local source_dir=$(realpath $(dirname "$script_path"))
